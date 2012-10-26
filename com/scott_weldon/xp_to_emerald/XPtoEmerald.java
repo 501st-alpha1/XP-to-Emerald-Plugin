aec07fac07a4d0f23133f40df053c5570ef07e5a
Scott Weldon <open-source@scott-weldon.com>
2012-10-26 12:05:17 -0700

    Removed showxp command
    On failure, xptoemerald now shows how much XP player has.
    Also testing adding of large item stacks.

--- com/scott_weldon/xp_to_emerald/XPtoEmerald.java
+++ com/scott_weldon/xp_to_emerald/XPtoEmerald.java
@@ -27,41 +27,30 @@ public final class XPtoEmerald extends JavaPlugin {
     if (cmd.getName().equalsIgnoreCase("xptoemerald")) {
       if (sender instanceof Player) {
         Player player = (Player) sender;
+        PlayerInventory inventory = player.getInventory();
+        // Test
+        inventory.addItem(new ItemStack(Material.DIAMOND, 65));
+        // End test
         // Permissions stuff here?
         int expToUse = Integer.parseInt(args[0]);
         int emeralds = expToUse / SCALE;
-        PlayerInventory inventory = player.getInventory();
+        if ((expToUse % SCALE) != 0) {
+          expToUse -= expToUse % SCALE;
+        }
         int exp = (int) player.getTotalExperience();
         if (expToUse > exp) {
-          sender.sendMessage("You don't have enough XP!");
+          sender.sendMessage("You only have " + exp + " XP!");
           return true;
         }
-        else {
-          player.giveExp(-expToUse);
-          while (emeralds > 0) {
-            ItemStack emeraldStack;
-            if (emeralds > 64) {
-              emeraldStack = new ItemStack(Material.EMERALD, 64);
-            }
-            else {
-              emeraldStack = new ItemStack(Material.EMERALD, emeralds);
-            }
-            emeralds -= 64;
-            inventory.addItem(emeraldStack);
-          }
+        player.setTotalExperience(exp - expToUse);
+        while (emeralds > 0) {
+          ItemStack emeraldStack;
+          int currentStack = (emeralds > 64) ? 64 : emeralds;
+          emeralds -= 64;
+          emeraldStack = new ItemStack(Material.EMERALD, currentStack);
+          inventory.addItem(emeraldStack);
         }
-        return true;
-      }
-      else {
-        sender.sendMessage("This command may not be used from the console.");
-        return false;
-      }
-    }
-    if (cmd.getName().equalsIgnoreCase("showxp")) {
-      if (sender instanceof Player) {
-        Player player = (Player) sender;
-        sender.sendMessage("Your total experience is: "
-            + player.getTotalExperience());
+
         return true;
       }
       else {
