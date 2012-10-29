32916c40ac65875235ef08d1c8d1823022cea08e
Scott Weldon <open-source@scott-weldon.com>
2012-10-29 12:26:03 -0700

    Refactored, added ability to use from console.
    Moved most of functionality to separate methods, to allow for conversion from
    multiple sources (currently player and console).

--- com/scott_weldon/xp_to_emerald/XPtoEmerald.java
+++ com/scott_weldon/xp_to_emerald/XPtoEmerald.java
@@ -1,6 +1,8 @@
 package com.scott_weldon.xp_to_emerald;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -9,10 +11,13 @@ import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class XPtoEmerald extends JavaPlugin {
+  private Server server;
+
   public static final int SCALE = 20;
 
   @Override
   public void onEnable() {
+    server = Bukkit.getServer();
   }
 
   @Override
@@ -21,13 +26,12 @@ public final class XPtoEmerald extends JavaPlugin {
 
   public boolean onCommand(CommandSender sender, Command cmd, String label,
       String[] args) {
-    if (args.length < 1 || args.length > 1) {
+    if (args.length < 1 || args.length > 2) {
       return false;
     }
     if (cmd.getName().equalsIgnoreCase("xptoemerald")) {
       if (sender instanceof Player) {
         Player player = (Player) sender;
-        PlayerInventory inventory = player.getInventory();
 
         if (!player.hasPermission("xptoemerald.convert")
             && !player.hasPermission("xptemerald.admin")
@@ -36,28 +40,30 @@ public final class XPtoEmerald extends JavaPlugin {
           return true;
         }
 
-        int expToUse = Integer.parseInt(args[0]);
-        int emeralds = expToUse / SCALE;
-        expToUse -= expToUse % SCALE;
-        int exp = player.getTotalExperience();
-        if (expToUse > exp) {
-          player.sendMessage("You only have " + exp + " XP!");
-          return true;
-        }
-        setTotalXP(player, exp - expToUse);
-        inventory.addItem(new ItemStack(Material.EMERALD, emeralds));
+        int xp = Integer.parseInt(args[0]);
 
-        return true;
+        return xpToEmerald(player, xp);
       }
       else {
-        sender.sendMessage("This command may not be used from the console.");
-        return true;
+        Player player = server.getPlayer(args[0]);
+
+        if (player == null) {
+          sender.sendMessage("Player " + args[0] + " does not exist!");
+          return false;
+        }
+        if (!player.isOnline()) {
+          sender.sendMessage("Player " + args[0] + " is not online!");
+          return true;
+        }
+
+        int xp = Integer.parseInt(args[1]);
+
+        return xpToEmerald(player, xp);
       }
     }
     if (cmd.getName().equalsIgnoreCase("emeraldtoxp")) {
       if (sender instanceof Player) {
         Player player = (Player) sender;
-        PlayerInventory inventory = player.getInventory();
 
         if (!player.hasPermission("xptoemerald.convert")
             && !player.hasPermission("xptemerald.admin")
@@ -67,36 +73,71 @@ public final class XPtoEmerald extends JavaPlugin {
         }
 
         int emeralds = Integer.parseInt(args[0]);
-        int emeraldId = Material.EMERALD.getId();
 
-        int numOfEmeralds = 0;
-        for (ItemStack item : inventory) {
-          if (item == null) {
-            continue;
-          }
-          if (item.getType() == Material.EMERALD) {
-            numOfEmeralds += item.getAmount();
-          }
-        }
-        inventory.remove(emeraldId);
-
-        if (numOfEmeralds < emeralds) {
-          player.sendMessage("You only have " + numOfEmeralds + " emeralds!");
-        }
-
-        int exp = player.getTotalExperience() + (emeralds * SCALE);
-        setTotalXP(player, exp);
-        inventory.addItem(new ItemStack(Material.EMERALD, numOfEmeralds
-            - emeralds));
-
-        return true;
+        return emeraldToXP(player, emeralds);
       }
       else {
-        sender.sendMessage("This command may not be used from the console.");
-        return true;
+        Player player = server.getPlayer(args[0]);
+
+        if (player == null) {
+          sender.sendMessage("Player " + args[0] + " does not exist!");
+          return false;
+        }
+        if (!player.isOnline()) {
+          sender.sendMessage("Player " + args[0] + " is not online!");
+          return true;
+        }
+
+        int emeralds = Integer.parseInt(args[1]);
+
+        return emeraldToXP(player, emeralds);
       }
     }
     return false;
+  }
+
+  public boolean xpToEmerald(Player player, int xp) {
+    PlayerInventory inventory = player.getInventory();
+
+    int emeralds = xp / SCALE;
+    xp -= xp % SCALE;
+    int exp = player.getTotalExperience();
+    if (xp > exp) {
+      player.sendMessage("You only have " + exp + " XP!");
+      return true;
+    }
+    setTotalXP(player, exp - xp);
+    inventory.addItem(new ItemStack(Material.EMERALD, emeralds));
+
+    return true;
+  }
+
+  public boolean emeraldToXP(Player player, int emeralds) {
+    PlayerInventory inventory = player.getInventory();
+
+    int emeraldId = Material.EMERALD.getId();
+
+    int numOfEmeralds = 0;
+    for (ItemStack item : inventory) {
+      if (item == null) {
+        continue;
+      }
+      if (item.getType() == Material.EMERALD) {
+        numOfEmeralds += item.getAmount();
+      }
+    }
+    inventory.remove(emeraldId);
+
+    if (numOfEmeralds < emeralds) {
+      player.sendMessage("You only have " + numOfEmeralds + " emeralds!");
+    }
+
+    int exp = player.getTotalExperience() + (emeralds * SCALE);
+    setTotalXP(player, exp);
+    inventory
+        .addItem(new ItemStack(Material.EMERALD, numOfEmeralds - emeralds));
+
+    return true;
   }
 
   public void setTotalXP(Player player, int xp) {
