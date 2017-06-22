9dadd76bbfaa3e6e9efd02d67c3db2df0821d83a
Scott Weldon <open-source@scott-weldon.com>
2017-06-22 15:11:17 -0700

    Add interfaces for Players and Inventories

---
+++ src/com/scott_weldon/xp_to_emerald/common/interfaces/Inventory.java
@@ -0,0 +1,42 @@
+/* Copyright (c) 2017 Scott Weldon
+ *
+ *  The XP to Emerald Plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The XP to Emerald Plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.scott_weldon.xp_to_emerald.common.interfaces;
+
+public interface Inventory {
+  /**
+   * Add the given items to the inventory.
+   *
+   * @param name    The name of the item type to add.
+   * @param number  How many items should be added.
+   */
+  public void addItems(String name, int number);
+
+  /**
+   * Get all of a given item from the inventory.
+   *
+   * @param name  The name of the item type to retrieve.
+   */
+  public int getItems(String name);
+
+  /**
+   * Remove the given items from the inventory.
+   *
+   * @param name    The name of the item type to remove.
+   * @param number  The number of items to remove.
+   */
+  public void removeItems(String name, int number);
+}
---
+++ src/com/scott_weldon/xp_to_emerald/common/interfaces/Player.java
@@ -0,0 +1,59 @@
+/* Copyright (c) 2017 Scott Weldon
+ *
+ *  The XP to Emerald Plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The XP to Emerald Plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.scott_weldon.xp_to_emerald.common.interfaces;
+
+public interface Player {
+  /**
+   * Get the name of the world the player is currently in.
+   */
+  public String getCurrentWorldName();
+
+  /**
+   * Get the display name for the player (for e.g. use in messages).
+   */
+  public String getDisplayName();
+
+  /**
+   * Get the percentage XP the player has towards the next level.
+   */
+  public float getExp();
+
+  /**
+   * Get the player's {@link Inventory}.
+   */
+  public Inventory getInventory();
+
+  /**
+   * Get the player's current level.
+   */
+  public int getLevel();
+
+  /**
+   * Send a message to the player.
+   */
+  public void sendMessage(String message);
+
+  /**
+   * Set the percentage XP the player has towards the next level.
+   */
+  public void setExp(float exp);
+
+  /**
+   * Set the player's current level.
+   */
+  public void setLevel(int level);
+}
