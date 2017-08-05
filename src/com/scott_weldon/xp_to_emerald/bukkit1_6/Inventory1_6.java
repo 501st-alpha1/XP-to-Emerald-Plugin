5afa1395fa44e4ca2b5d009315669d9f574d143d
Scott Weldon <open-source@scott-weldon.com>
2017-08-05 16:05:53 -0700

    Implement Bukkit 1.6 player/inventory classes

---
+++ src/com/scott_weldon/xp_to_emerald/bukkit1_6/Inventory1_6.java
@@ -0,0 +1,67 @@
+/* Copyright (c) 2017 Scott Weldon
+ *
+ *  The XP to Emerald Plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The XP to Emerald Plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.scott_weldon.xp_to_emerald.bukkit1_6;
+
+import java.lang.IllegalArgumentException;
+import com.scott_weldon.xp_to_emerald.common.interfaces.PlayerInterface;
+import com.scott_weldon.xp_to_emerald.common.interfaces.InventoryInterface;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.Material;
+
+public class Inventory1_6 implements InventoryInterface {
+  private PlayerInventory bukkitInventory;
+
+  public Inventory1_6(PlayerInventory bukkitInventory) {
+    this.bukkitInventory = bukkitInventory;
+  }
+
+  public void addItems(String name, int number) {
+    Material material = Material.getMaterial(name.toUpperCase());
+    if (material == null) {
+      throw new IllegalArgumentException("Invalid material.");
+    }
+
+    this.bukkitInventory.addItem(new ItemStack(material, number));
+  }
+
+  public int getItems(String name) {
+    Material material = Material.getMaterial(name.toUpperCase());
+    if (material == null) {
+      throw new IllegalArgumentException("Invalid material.");
+    }
+
+    int numOfEmeralds = 0;
+    for (ItemStack item : bukkitInventory) {
+      if (item == null) {
+        continue;
+      }
+
+      if (item.getType() == material) {
+        numOfEmeralds += item.getAmount();
+      }
+    }
+
+    return numOfEmeralds;
+  }
+
+  public void removeItems(String name, int number) {
+    if (number < this.getItems(name)) {
+      throw new IllegalArgumentException("Cannot remove more items than exist.");
+    }
+  }
+}
---
+++ src/com/scott_weldon/xp_to_emerald/bukkit1_6/Player1_6.java
@@ -0,0 +1,60 @@
+/* Copyright (c) 2017 Scott Weldon
+ *
+ *  The XP to Emerald Plugin is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The XP to Emerald Plugin is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.scott_weldon.xp_to_emerald.bukkit1_6;
+
+import com.scott_weldon.xp_to_emerald.common.interfaces.PlayerInterface;
+import com.scott_weldon.xp_to_emerald.common.interfaces.InventoryInterface;
+
+public class Player1_6 implements PlayerInterface {
+  private org.bukkit.entity.Player bukkitPlayer;
+
+  public Player1_6(org.bukkit.entity.Player bukkitPlayer) {
+    this.bukkitPlayer = bukkitPlayer;
+  }
+
+  public String getCurrentWorldName() {
+    return this.bukkitPlayer.getLocation().getWorld().getName();
+  }
+
+  public String getDisplayName() {
+    return this.bukkitPlayer.getDisplayName();
+  }
+
+  public float getExp() {
+    return this.bukkitPlayer.getExp();
+  }
+
+  public InventoryInterface getInventory() {
+    return new Inventory1_6(bukkitPlayer.getInventory());
+  }
+
+  public int getLevel() {
+    return this.bukkitPlayer.getLevel();
+  }
+
+  public void sendMessage(String message) {
+    this.bukkitPlayer.sendMessage(message);
+  }
+
+  public void setExp(float exp) {
+    this.bukkitPlayer.setExp(exp);
+  }
+
+  public void setLevel(int level) {
+    this.bukkitPlayer.setLevel(level);
+  }
+}
